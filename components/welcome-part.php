021385b9c318d294da9bff56e4d611d23892922e
Sébastien Quirrion <sebastien@fitlab.fr>
2022-06-23 11:11:11 +0200

    Ajout titre, baseline et citation

--- components/welcome-part.php
+++ components/welcome-part.php
@@ -6,10 +6,18 @@
 <div class="fr-container">
   <div class="fr-grid-row fr-mt-12w">
     <div class="fr-col-xs-12 fr-col-md-8 fr-pr-xs-1w fr-pr-md-6w">
-      <h1 class="fr-h1">Actualit&eacute;s du dispositif<br />Conseiller num&eacute;rique France Services</h1> 
+      <h1 class="fr-h1">Les chemins du num&eacute;rique</h1> 
       <p style="color:#929292">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        Rencontres au c&oelig;ur du num&eacute;rique inclusif
       </p>
+      <figure class="fr-quote fr-quote--column" style="margin-left: 2rem;">
+        <blockquote>
+            <p style="font-size:14px;line-height:1.75rem">&laquo;&nbsp;L&rsquo;inclusion num&eacute;rique s&rsquo;impose aujourd&rsquo;hui comme une priorit&eacute; des pouvoirs publics, afin de r&eacute;duire la fracture num&eacute;rique aupr&egrave;s de la population. Les conseillers num&eacute;riques France Services sont une r&eacute;elle opportunit&eacute; pour les usagers qui souhaitent devenir autonomes sur le num&eacute;rique du quotidien. Les chemins du num&eacute;riques explorent les diff&eacute;rentes incarnations de cette nouvelle offre de service public, qui prend vie gr&acirc;ce au dispositif Conseiller num&eacute;rique France Services.&nbsp;&raquo;</p>
+        </blockquote>
+        <figcaption>
+            <p class="fr-quote__author" style="font-size:12px;">Pierre-Louis Rolle, Directeur Strat&eacute;gie Innovation &agrave; la Direction G&eacute;n&eacute;rale D&eacute;l&eacute;gu&eacute;e au Num&eacute;rique de l&rsquo;ANCT</p>
+        </figcaption>
+    </figure> 
     </div>
     <div class="fr-col-xs-12 fr-col-md-4">
       <?php require 'tags.php'; ?>
